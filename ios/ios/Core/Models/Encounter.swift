c402568f17e9e7f948bd9ac69a88b6f8dd96345a
ryusuke1006 <r-kawata@digeon.co>
2026-03-19 00:59:23 +0900

    fix: avoid grouping older encounters under today

--- ios/ios/Core/Models/Encounter.swift
+++ ios/ios/Core/Models/Encounter.swift
@@ -7,23 +7,37 @@ struct Encounter: Identifiable, Hashable {
     let relativeTime: String
     let lyric: String
 
+    var happenedToday: Bool {
+        relativeTime == "たった今"
+            || relativeTime.hasSuffix("分前")
+            || relativeTime.hasSuffix("時間前")
+            || relativeTime == "近日"
+    }
+
     var happenedYesterday: Bool {
         relativeTime == "昨日"
+    }
+
+    var happenedEarlier: Bool {
+        !happenedToday && !happenedYesterday
     }
 }
 
 enum EncounterSection: String, CaseIterable, Identifiable {
     case today = "今日"
     case yesterday = "昨日"
+    case earlier = "以前"
 
     var id: String { rawValue }
 
     func includes(_ encounter: Encounter) -> Bool {
         switch self {
         case .today:
-            return !encounter.happenedYesterday
+            return encounter.happenedToday
         case .yesterday:
             return encounter.happenedYesterday
+        case .earlier:
+            return encounter.happenedEarlier
         }
     }
 }
